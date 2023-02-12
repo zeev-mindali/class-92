efd06090d078c1e76331df1b77ae9b37baf68cd0
zeev mindali <mindalizeev@gmail.com>
2023-02-12 10:57:06 +0200

    form handle

--- javascript/12_02/js/friends.js
+++ javascript/12_02/js/friends.js
@@ -1,5 +1,19 @@
 var friendList = [];
 
 const getData = () => {
-  alert("it's working");
+  var fName = document.getElementById("fName").value;
+  var fTel = document.getElementById("fTel").value;
+  var fPhoto = document.getElementById("fImg").value;
+
+  console.log(fName, fTel, fPhoto);
+
+  var newFriend = new Object();
+  newFriend.name = fName;
+  newFriend.tel = fTel;
+  newFriend.photo = fPhoto;
+
+  friendList.push(newFriend);
+
+  console.log(friendList);
+  document.getElementById("friendForm").reset();
 };
