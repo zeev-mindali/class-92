82a7cb1eede433a6a844635160ed08ce5b14ca8c
zeev mindali <mindalizeev@gmail.com>
2023-02-19 10:08:09 +0200

    exam

--- exam_prep_19_02/js/myJS.js
+++ exam_prep_19_02/js/myJS.js
@@ -20,12 +20,13 @@ document.getElementById("studentTable").innerHTML = result;
 
 const getName = () => {
   var userName = document.getElementById("uName").value;
-  var myRes = document.getElementById("res");
+
   var result = "";
   if (userName.includes("ran")) {
     result += `<h1>Here is another lie of ${userName}<h1><hr/>`;
   }
   result += `<h1>${userName} is so smart, even bibi is learning how to run a country from ${userName}<h1>`;
-  myRes.innerHTML = result;
+  document.getElementById("res").innerHTML = result;
+  document.getElementById("myForm").reset();
   //userName.value = "";
 };
