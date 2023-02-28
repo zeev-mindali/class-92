741f36472522fed588e274c1bfcf0f5a6eb701a1
zeev mindali <mindalizeev@gmail.com>
2023-02-28 10:10:47 +0200

    lpr

--- advanced js/26.02/myAPI/js/myJS.js
+++ advanced js/26.02/myAPI/js/myJS.js
@@ -1,6 +1,5 @@
 const url =
-  "https://data.gov.il/api/3/action/datastore_search?resource_id=9ad3862c-8391-4b2f-84a4-2d4c68625f4b&limit=5000&q='ירושלים'"; //end point
-var street = [];
+  "https://data.gov.il/api/3/action/datastore_search?resource_id=9ad3862c-8391-4b2f-84a4-2d4c68625f4b&limit=5000&q='ירושלים'"; //end point  
 
 // var myData = fetch(url)
 //   .then((response) => response.json())
---
+++ advanced js/28.02/js/myJS.js
@@ -0,0 +1,32 @@
+const url =
+  "https://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3&q=";
+
+const findCar = () => {
+  //url -> end point - נקודת קצה
+  //which data we want to sent
+  const lp = document.getElementById("lp").value;
+  const myData = fetch(url + lp)
+    .then((response) => response.json())
+    .then((data) => displayInfo(data.result.records[0]));
+};
+
+const displayInfo = (carData) => {
+  //where we want to sent it, or present it....
+  console.log(carData);
+  document.getElementById("res").innerHTML = `
+    <h1>${carData.mispar_rechev}</h1><hr/>
+    <h2>${carData.tozeret_nm}</h2>
+    <h3>${carData.kinuy_mishari} ${carData.ramat_gimur}</h3>
+    <h3>${carData.tzeva_rechev}</h3>
+    <h3>${carData.shnat_yitzur}</h3>
+    <hr/>
+    <h1>${handleDate(carData.tokef_dt)}</h1>
+  `;
+};
+
+// 0   1  2
+//2023-07-11
+const handleDate = (oldDate) => {
+  const ymd = oldDate.split("-");
+  return ymd[2] + "/" + ymd[1] + "/" + ymd[0];
+};
