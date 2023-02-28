0cec75b70ff186f7fb49bc5f73c1d5c748d23467
zeev mindali <mindalizeev@gmail.com>
2023-02-28 15:37:36 +0200

    streets

--- advanced js/26.02/myAPI/js/myJS.js
+++ advanced js/26.02/myAPI/js/myJS.js
@@ -1,5 +1,5 @@
-const url =
-  "https://data.gov.il/api/3/action/datastore_search?resource_id=9ad3862c-8391-4b2f-84a4-2d4c68625f4b&limit=5000&q='ירושלים'"; //end point  
+// const url =
+//   "https://data.gov.il/api/3/action/datastore_search?resource_id=9ad3862c-8391-4b2f-84a4-2d4c68625f4b&limit=5000&q='ירושלים'"; //end point
 
 // var myData = fetch(url)
 //   .then((response) => response.json())
@@ -13,13 +13,30 @@ const url =
 //     document.getElementById("container").innerHTML = result;
 //   });
 
-var myData = fetch(url)
+const urlStreet =
+  "https://data.gov.il/api/3/action/datastore_search?resource_id=9ad3862c-8391-4b2f-84a4-2d4c68625f4b&limit=5000&q="; //end point
+
+const urlCity =
+  "https://data.gov.il/api/3/action/datastore_search?resource_id=5c78e9fa-c2e2-4771-93ff-7f400a12f7ba&limit=1500";
+
+const myCities = fetch(urlCity)
   .then((response) => response.json())
   .then((data) => {
-    street = data.result.records;
-    console.log("me ready");
     result = "";
-    street.map((item) => (result += `<option>${item["שם_רחוב"]}</option>`));
-    document.getElementById("street").innerHTML = result;
+    var myCities = data.result.records;
+    myCities.map((item) => (result += `<option>${item["שם_ישוב"]}</option>`));
+    document.getElementById("city").innerHTML = result;
   });
-console.log("sync:", myData);
+
+const getStreet = () => {
+  var city = document.getElementById("city").value;
+  var myData = fetch(urlStreet + city)
+    .then((response) => response.json())
+    .then((data) => {
+      street = data.result.records;
+      console.log("me ready");
+      result = "";
+      street.map((item) => (result += `<option>${item["שם_רחוב"]}</option>`));
+      document.getElementById("street").innerHTML = result;
+    });
+};
