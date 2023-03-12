a6c739311dc952203a8a32d7c1a849f21aea65be
zeev mindali <mindalizeev@gmail.com>
2023-03-12 09:21:17 +0200

    disco light

---
+++ advanced js/12.03/hw/js/myJS.js
@@ -0,0 +1,25 @@
+const colors = [
+  "azure",
+  "red",
+  "green",
+  "yellow",
+  "brown",
+  "blue",
+  "orange",
+  "pink",
+  "purple",
+  "black",
+];
+
+var colorRunner = setInterval(() => {
+  document.body.style.backgroundColor =
+    colors[Math.floor(Math.random() * colors.length)];
+}, 500);
+
+const changeSpeed = (myValue) => {
+  clearInterval(colorRunner);
+  var colorRunner = setInterval(() => {
+    document.body.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+  }, myValue);
+};
