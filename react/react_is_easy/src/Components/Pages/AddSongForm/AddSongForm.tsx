85c0a402e794e871c6a567806f7cf6a0bb8dbc2d
galamouyal88@gmail.com <galamouyal88@gmail.com>
2023-04-16 10:03:14 +0300

    getting song data

--- react/react_is_easy/src/Components/Pages/AddSongForm/AddSongForm.tsx
+++ react/react_is_easy/src/Components/Pages/AddSongForm/AddSongForm.tsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
 import "./AddSongForm.css";
+import axios from "axios";
 
 function AddSongForm(): JSX.Element {
+    //demo song=> https://www.youtube.com/watch?v=Ggafij3sZ1g
     const [songURL,setURL] = useState("");
     const apiKey = "AIzaSyC89Q2JGf-GbWN25SGjoW69l4VnDTHlzXY";
     const apiURL = `https://www.googleapis.com/youtube/v3/videos?part=snippet&key=${apiKey}&id=`;
     const searchSong = ()=>{
-        
+        //console.log(songURL.split("=")[1]);
+        const songID = songURL.split("=")[1];
+        axios.get(apiURL+songID)
+        .then(response=>{
+            console.log(response);
+        })
     }
 
     return (
