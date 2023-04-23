3089578aaa28c63ede7b4b61871861ffc3e772a7
zeev mindali <mindalizeev@gmail.com>
2023-04-23 15:00:52 +0300

    mui

--- react/react_is_easy/src/Components/Pages/EditSong/EditSong.tsx
+++ react/react_is_easy/src/Components/Pages/EditSong/EditSong.tsx
@@ -3,6 +3,8 @@ import "./EditSong.css";
 import { useEffect, useState } from "react";
 import Song from "../../modal/Song";
 import { youtube } from "../../Redux/Store";
+import { Button, ButtonGroup, TextField, Typography } from "@mui/material";
+import { Cancel, Send } from "@mui/icons-material";
 
 function EditSong(): JSX.Element {
   const params = useParams();
@@ -18,23 +20,57 @@ function EditSong(): JSX.Element {
   return (
     <div className="EditSong">
       <div className="Box" style={{ width: 300 }}>
-        song title
-        <br />
+        <Typography variant="h3" className="HeadLine">
+          Edit song
+        </Typography>
+        <hr />
+        {/*song title
         <input type="text" value={song?.title} />
+        */}
+
+        <TextField name="song title" variant="outlined" className="TextBox" />
         <br />
-        <br />
+        {/*        
         song description
         <br />
         <input type="text" value={song?.descption} />
         <br />
+        */}
+        <TextField
+          name="song description"
+          variant="outlined"
+          className="TextBox"
+        />
         <br />
+
+        <br />
+        {/*
         song image
         <br />
         <input type="text" value={song?.img} />
         <br />
+        */}
+        <TextField
+          name="song image"
+          variant="outlined"
+          className="TextBox"
+          type="url"
+        />
+        <br />
+        <br />
+        {/*
         <br />
         <input type="submit" />
         <input type="reset" />
+        */}
+        <ButtonGroup variant="contained" fullWidth>
+          <Button color="primary" startIcon={<Send />}>
+            Update
+          </Button>
+          <Button color="secondary" startIcon={<Cancel />}>
+            Cancel
+          </Button>
+        </ButtonGroup>
       </div>
     </div>
   );
