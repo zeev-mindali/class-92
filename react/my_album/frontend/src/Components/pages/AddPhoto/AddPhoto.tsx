38839d973192dd038166d19e6713c1ef48c0b581
zeev mindali <mindalizeev@gmail.com>
2023-05-02 13:34:53 +0300

    photo handler

--- react/my_album/frontend/src/Components/pages/AddPhoto/AddPhoto.tsx
+++ react/my_album/frontend/src/Components/pages/AddPhoto/AddPhoto.tsx
@@ -4,11 +4,17 @@ import "./AddPhoto.css";
 import { useForm } from "react-hook-form";
 import { Photo } from "../../Modal/Photo";
 import { addPhotoAction } from "../../redux/PhotosReducer";
+import { useNavigate } from "react-router-dom";
 
 function AddPhoto(): JSX.Element {
   //URL, descrption, category, current date (disabled), time (disabled)
   const [clock, setClock] = useState("");
-
+  const [imageURL, setURL] = useState("");
+  const navigate = useNavigate();
+  const previewStyle = {
+    width: 300,
+    margin: 10,
+  };
   //form
   const {
     register,
@@ -27,6 +33,7 @@ function AddPhoto(): JSX.Element {
       "photos",
       JSON.stringify(store.getState().photos.allPhotos)
     );
+    navigate("/");
   };
 
   useEffect(() => {
@@ -39,30 +46,54 @@ function AddPhoto(): JSX.Element {
   const myDesign = { marginTop: 50, width: 300 };
 
   return (
-    <div className="AddPhoto Box" style={myDesign}>
-      <form onSubmit={handleSubmit(send)}>
-        <h3>Add Photo</h3>
-        <hr />
-        <input type="url" placeholder="enter image url" {...register("URL")} />
-        <br />
-        <input
-          type="text"
-          placeholder="enter image description"
-          {...register("description")}
-        />
-        <br />
-        <select {...register("category")}>
-          <option disabled>Choose Category</option>
-          {store.getState().category.categories.map((item) => (
-            <option>{item.name}</option>
-          ))}
-        </select>
-        <br />
-        {new Date().toDateString()} <br />
-        {clock}
-        <br />
-        <button>Add Photo</button>
-      </form>
+    <div
+      style={{ display: "flex", justifyContent: "center" }}
+      className="AddPhoto"
+    >
+      <div className="Box" style={myDesign}>
+        <form onSubmit={handleSubmit(send)}>
+          <h3>Add Photo</h3>
+          <hr />
+          <input
+            type="url"
+            placeholder="enter image url"
+            {...register("URL")}
+            onBlur={(e) => setURL(e.target.value)}
+          />
+          <br />
+          {/* <input
+            type="text"
+            placeholder="enter image description"
+            {...register("description")}
+          /> */}
+          <textarea
+            placeholder="enter image description"
+            rows={3}
+            style={{ width: 300 }}
+            {...register("description")}
+          />
+          <br />
+          <select {...register("category")}>
+            <option disabled>Choose Category</option>
+            {store.getState().category.categories.map((item) => (
+              <option>{item.name}</option>
+            ))}
+          </select>
+          <br />
+          {new Date().toDateString()} <br />
+          {clock}
+          <br />
+          <button>Add Photo</button>
+        </form>
+      </div>
+      <div className="Box" style={myDesign}>
+        {imageURL && (
+          <div>
+            <h3>Image Preview</h3>
+            <img src={imageURL} alt={imageURL} style={previewStyle} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
