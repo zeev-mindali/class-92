20c73b0939cd0b6801d1832bc18f6778463210b9
zeev mindali <mindalizeev@gmail.com>
2023-05-07 11:52:20 +0300

    web server function

--- backend/hello_backend/Functions/DateFunctions.js
+++ backend/hello_backend/Functions/DateFunctions.js
@@ -1,4 +1,6 @@
 exports.myBday = (bYear) => {
-  console.log("i am in date function");
-  return new Date().getFullYear() - bYear.split("/")[2];
+  //console.log("i am in date function");
+  return new Date().getFullYear() - bYear.split("/")[3];
+  console.log(bYear.split("/")[3]);
+  //return "test";
 };
--- backend/hello_backend/webserver.js
+++ backend/hello_backend/webserver.js
@@ -1,15 +1,20 @@
 var http = require("http");
 var httpPort = 8080;
 var dt = require("./Functions/DateFunctions");
-
+/*
+/29/08/2000
+01234567890
+ 1        0
+ */
 http
   .createServer((request, response) => {
     response.writeHead(200, { "Content-Type": "text/html" });
     response.write("<h1><center>Age calculator</center></h1><hr/>");
     response.write(
-      `Hello zeev, you born in <b><u>08/07/1974</u></b> and you are <b><u>${dt.myBday(
-        "08/07/1974"
-      )}</u></b> years old`
+      `Hello zeev, you born in <b><u>${request.url.substring(
+        1,
+        request.url.length
+      )}</u></b> and you are <b><u>${dt.myBday(request.url)}</u></b> years old`
     );
     //response.end("<hr/><center>All rights reseved to class 92</center>");
     response.end("<hr/> your url: " + request.url);
