261d95163f7025e30b8cdc06b9004b329a83aced
zeev mindali <mindalizeev@gmail.com>
2023-05-28 15:33:02 +0300

    youtube matan

--- express/youtube/backend/Utils/Config.ts
+++ express/youtube/backend/Utils/Config.ts
@@ -2,7 +2,7 @@ class Config {
   public mysql_host = "localhost";
   public mysql_user = "root";
   public mysql_password = "12345678";
-  public mysql_port = 3316;
+  public mysql_port = 3306;
   public mysql_database = "youtube";
   public webPort = 4000;
 }
