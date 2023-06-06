2759445e3674b74edae893f6e32f055f4ea3d975
zeev mindali <mindalizeev@gmail.com>
2023-06-06 11:14:32 +0300

    binding

--- angular/youtube/src/app/Layout/main/main.component.ts
+++ angular/youtube/src/app/Layout/main/main.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-main',
-  templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+    selector: 'app-main',
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+    title = "Song List";
+    groom = "Matan";
+    bride = "Tamar";
+    myType = "text";
+    myValue = "";
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
+    showLoved() {
+        return this.groom + " wants to merry " + this.bride;
+    }
+
+    onPassUpdate(event: any) {
+        this.myValue = event.target.value;
+        console.log(event.target.value);
+    }
 }
