d6e6aa2bba26a625ce2b9c5e1914f7c1ca4459ce
zeev mindali <mindalizeev@gmail.com>
2023-07-02 09:29:55 +0300

    our first test

---
+++ jest/examples/sum.js
@@ -0,0 +1,10 @@
+const sum = (num1, num2) => {
+    //return the sum of num1 + num2
+    return num1 + num2;
+}
+
+const helper = () => {
+    console.log("hello world from helper");
+}
+
+module.exports = sum;
